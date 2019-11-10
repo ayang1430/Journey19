99208113b55b9df267a3815bdf5b3e1e72058404
Alice <ayang1430@gmail.com>
2019-11-10 10:14:54 -0500

    all of the things

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var fruits = ["apple", "banana", "orange"];
+
+var object = fruits.pop();
+
+var firstElement = fruits[0];
+
+var fruits2 = ["pineapple", "mango"];
+
+fruits.push(firstElement);
+
+fruits = fruits.concat(["strawberry"]);
+
+fruitsLast = fruits.pop();
+
+console.log(fruits);
+
+fruits[3] = ("strawberry");
+
+console.log(fruits);
+
+//write code to access the first element of the array
+
+
+//write code to change an element of the array
+
+
+//write code to create a new array with new objects
+
+
+//write code to combine fruits and fruits2
+
+
+
+
+var mom = {
+        name: "Linda",
+        home: "Virginia",
+        born: "New York",
+        personality: ["kind", "funny", "leader", "smart"],
+}
+
+console.log(mom);
+console.log(mom.name);
+console.log(mom.personality);
